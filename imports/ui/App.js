fc914b7b1bc5907cf3afd04df05723654eba8948
l.boger <l.boger@betzold.de>
2018-11-19 15:44:15 +0100

    Tutorial ToDo-List step: 7.3

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,60 +1,80 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import { withTracker } from 'meteor/react-meteor-data';
+ import React, { Component } from 'react';
+ import ReactDOM from 'react-dom';
+ import { withTracker } from 'meteor/react-meteor-data';
+ 
+ import { Tasks } from '../api/tasks.js';
+ 
+ import Task from './Task.js';
+ 
+ // App component - represents the whole app
+ class App extends Component {
+   constructor(props) {
+     super(props);
 
-import { Tasks } from '../api/tasks.js';
+     this.state = {
+       hideCompleted: false,
+     };
+   }
+   handleSubmit(event) {
+     event.preventDefault();
+ 
+     // Find the text field via the React ref
+     const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+ 
+     Tasks.insert({
+       text,
+       createdAt: new Date(), // current time
+     });
+ 
+     // Clear form
+     ReactDOM.findDOMNode(this.refs.textInput).value = '';
+   }
 
-import Task from './Task.js';
-
-// App component - represents the whole app
-class App extends Component {
-  handleSubmit(event){
-    event.preventDefault();
-
-    //Find the test field via the React ref
-    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
-
-    Tasks.insert({
-      text,
-      createdAt: new Date(), //current time
-    });
-
-    //Clear form
-    ReactDOM.findDOMNode(this.refs.textinput).value = '';
-  }
-
-  renderTasks() {
-    return this.props.tasks.map((task) => (
-      <Task key={task._id} task={task} />
-
-    ));
-  }
-
-  render() {
-    return (
-      <div className="container">
+   toggleHideCompleted() {
+     this.setState({
+       hideCompleted: !this.state.hideCompleted,
+     });
+   }
+ 
+   renderTasks() {
+     return this.props.tasks.map((task) => (
+       <Task key={task._id} task={task} />
+     ));
+   }
+ 
+   render() {
+     return (
+       <div className="container">
         <header>
           <h1>Todo List</h1>
-
-          <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+           <label className="hide-completed">
+            <input
+              type="checkbox"
+              readOnly
+              checked={this.state.hideCompleted}
+              onClick={this.toggleHideCompleted.bind(this)}
+            />
+            Hide Completed Tasks
+          </label>
+           <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
             <input
               type="text"
-              ref="textInput"
-              placeholder="Type to add new Tasks"
-            />
-          </form>
-        </header>
-
-        <ul>
-          {this.renderTasks()}
-        </ul>
-      </div>
-    );
-  }
-}
-
-export default withTracker(() => {
-  return {
-    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
-  };
-})(App);
+               ref="textInput"
+               placeholder="Type to add new tasks"
+             />
+           </form>
+         </header>
+ 
+         <ul>
+           {this.renderTasks()}
+         </ul>
+       </div>
+     );
+   }
+ }
+ 
+ export default withTracker(() => {
+   return {
+     tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+   };
+ })(App);
