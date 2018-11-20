eb7d4f1f9174572e278ecac57bd66ca257772998
l.boger <l.boger@betzold.de>
2018-11-20 08:23:10 +0100

    Tutorial ToDo-List step: 9.4 (removed insecure, defined methods in tasks.js to solve the problem that the input bottons won't work after the delete of insecure)

--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-
+import { Meteor } from 'meteor/meteor';
 import { Tasks } from '../api/tasks.js';
 
 // Task component - represents a single todo item
 export default class Task extends Component {
   toggleChecked(){
     //Set the checked property to the opposite of its current value
-    Tasks.update(this.props.task._id, {
-      $set: { checked: !this.props.task.checked },
-    });
+    Meteor.call('tasks.setChecked', this.props.task._id, !this.props.task.checked);
   }
 
-  deleteThisTask(){
-    Tasks.remove(this.props.task._id);
+  deleteThisTask() {
+    Meteor.call('tasks.remove', this.props.task._id);
   }
+  
   render() {
     // Give tasks a different className when they are checked off,
     // so that we can style them nicely in CSS
